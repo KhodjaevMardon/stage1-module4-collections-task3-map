7d7ce761ef9ff0c99d140cf1c909bdf5aa229910
Mardon Khodjaev <zombiee4407@gmail.com>
2023-01-22 23:04:56 +0500

    finished the task

--- src/main/java/com/epam/mjc/collections/map/KeyValueSwapper.java
+++ src/main/java/com/epam/mjc/collections/map/KeyValueSwapper.java
@@ -1,8 +1,16 @@
 package com.epam.mjc.collections.map;
 
+import java.util.HashMap;
 import java.util.Map;
 
 public class KeyValueSwapper {
     public Map<String, Integer> swap(Map<Integer, String> sourceMap) {
+        Map<String, Integer> resultMap = new HashMap<>();
+        for (Map.Entry<Integer, String> e : sourceMap.entrySet()) {
+            Integer initial = resultMap.getOrDefault(e.getValue(), Integer.MAX_VALUE);
+            resultMap.put(e.getValue(), Math.min(initial, e.getKey()));
+        }
+
+        return resultMap;
     }
 }
--- src/main/java/com/epam/mjc/collections/map/WordRepetitionMapCreator.java
+++ src/main/java/com/epam/mjc/collections/map/WordRepetitionMapCreator.java
@@ -1,8 +1,17 @@
 package com.epam.mjc.collections.map;
 
+import java.util.HashMap;
 import java.util.Map;
 
 public class WordRepetitionMapCreator {
     public Map<String, Integer> createWordRepetitionMap(String sentence) {
+        Map<String, Integer> resultMap = new HashMap<>();
+        for (String word : sentence.trim().split("\\W+")) {
+            if (word.isEmpty()) continue;
+
+            resultMap.put(word.toLowerCase(), resultMap.getOrDefault(word.toLowerCase(), 0) + 1);
+        }
+
+        return resultMap;
     }
 }
